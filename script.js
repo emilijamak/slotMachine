a52044afa776a62c31942809cafe8f02c39259f3
Name Surname <email@email.com>
2024-05-15 11:31:18 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const innerReels = document.querySelectorAll('.innerReel')
+const btn = document.querySelector('.btn')
+
+const symbolArr = ['🍋', '🍒', '🍇']
+btn.onclick = () => {
+    innerReels.forEach(innerReels => {
+
+    })
+}
+
+
